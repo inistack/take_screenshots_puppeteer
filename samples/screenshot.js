e82f4055bacd9f186cb49a7d170e216c0a7d8f5d
Ini Arthur <arthurshur@gmail.com>
2025-09-17 03:59:34 +0000

    connector

--- samples/screenshot.js
+++ samples/screenshot.js
@@ -5,16 +5,35 @@ import { remoteBrowserPage } from "./connector.js";
 
     console.log('Starting navigation');
 
-    // Navigate the page to a URL
-    await page.goto('https://www.lambdatest.com');
-
     // Take screenshot
-    await page.screenshot({ path: './lambdatest.png' });
+    await page.goto('https://www.lambdatest.com');
+    await page.screenshot({ path: 'lambdatest.png' });
     console.log('Screenshot taken!')
 
-    // Generate PDF
-    await page.pdf({ path: './lambdatest.pdf' });
-    console.log('PDF generated!')
+    // Take full page screenshot
+    await page.goto('https://www.lambdatest.com');
+    await page.screenshot({ path: 'lambdatest_fullpage.png', fullPage:true });
+    console.log('Full page screenshot taken!')
+
+    // Take screenshot of specific area of the page
+    await page.goto('https://www.lambdatest.com');
+    await page.screenshot({ path: "area_snap.png", clip: { x: 0, y: 70, width: 500, height: 700 } })
+    console.log('Screenshot of specific area taken!')
+
+    // Take screenshot of specific element
+    await page.goto('https://www.lambdatest.com/selenium-playground/table-pagination-demo');
+    const div = await page.$('#table-id');
+    await div.screenshot({ path: 'table.png' });
+    console.log('Screenshot of specific element taken!')
+
+    // Take screenshot of element with class name
+    await page.goto('https://www.lambdatest.com/selenium-playground/simple-form-demo');
+    const divs = await page.$$('.h-35');
+    for (let i = 0; i < divs.length; i++) {
+        await divs[i].screenshot({ path: `element${i}.png` });
+    }
+    console.log('Screenshots of elements with class name taken!')
+
 
     // set test status to passed
     await page.evaluate(_ => { }, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: "Test Passed" } })}`);
