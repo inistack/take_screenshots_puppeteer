9d5f14ccdfae6b1a41c8312fee345be04b125784
Iniubong Arthur <arthurshur@gmail.com>
2025-11-14 21:35:30 +0000

    changed view port

--- samples/screenshot.js
+++ samples/screenshot.js
@@ -17,7 +17,7 @@ import { remoteBrowserPage } from "./connector.js";
 
     // Take screenshot of specific area of the page
     await page.goto('https://www.lambdatest.com');
-    await page.screenshot({ path: "area_snap.png", clip: { x: 0, y: 70, width: 500, height: 700 } })
+    await page.screenshot({ path: "area_snap.png", clip: { x: 0, y: 80, width: 600, height: 800 } })
     console.log('Screenshot of specific area taken!')
 
     // Take screenshot of specific element
